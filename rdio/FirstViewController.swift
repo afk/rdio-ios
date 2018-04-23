d2808e3413ec3cee7101fbb7669db1bffc6036ea
Tim Wattenberg <mail@timwattenberg.de>
2018-04-23 22:40:25 +0200

    Initial Commit

---
+++ rdio/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  rdio
+//
+//  Created by Tim Wattenberg on 23.04.18.
+//  Copyright © 2018 Tim Wattenberg. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
